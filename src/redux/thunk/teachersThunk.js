83a72f4ff76df68d8b5015e3a3a15e9ac0f90e95
delicelydia <Delydia84@gmail.com>
2021-10-11 17:43:24 +0200

    Add reservation reducers

---
+++ src/redux/thunk/teachersThunk.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import {
+  apiCallStart,
+  loadTeachersSuccess,
+  loadTeachersFail,
+} from "../actions/actionCreators";
+
+const api = "http://localhost:3000/api/v1";
+
+export const loadTeachersAsync = () => async (dispatch) => {
+  dispatch(apiCallStart());
+  try {
+    const response = await axios.get(`${api}/teachers`, {
+      headers: {
+        Authorization: JSON.parse(localStorage.getItem("auth_token")),
+      },
+    });
+    console.log(response.data);
+    dispatch(loadTeachersSuccess(response.data));
+  } catch (error) {
+    dispatch(
+      loadTeachersFail(
+        error.response ? error.response.data.errors[0] : "Something went wrong"
+      )
+    );
+  }
+};
+
+export const loadfail = "";
