ee81d6d0af86faaa07c8353c84f7bd10ed73d413
delicelydia <Delydia84@gmail.com>
2021-10-25 18:13:56 +0200

    Add heroku link

--- src/redux/thunk/teachersThunk.js
+++ src/redux/thunk/teachersThunk.js
@@ -5,7 +5,7 @@ import {
   loadTeachersFail,
 } from '../actions/actionCreators';
 
-const api = 'http://localhost:3000/api/v1';
+const api = 'https://lydia-online-class.herokuapp.com/api/v1';
 
 export const loadTeachersAsync = () => async (dispatch) => {
   dispatch(apiCallStart());
