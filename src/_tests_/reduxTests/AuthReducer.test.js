9371bbcdcd92fe09ed5f0ea6b0634f4c8132e084
delicelydia <Delydia84@gmail.com>
2021-11-04 13:19:45 +0200

    fix linters

--- src/_tests_/reduxTests/AuthReducer.test.js
+++ src/_tests_/reduxTests/AuthReducer.test.js
@@ -3,67 +3,66 @@ import createStore from '../../redux/createStore';
 const store = createStore();
 
 describe('Auth reducer', () => {
-    it('should return the initial state', () => {
-      expect(store.getState().auth).toEqual({
-        currentUser: null,
-        isAuthenticated: false,
-        loading: false,
-        error: null,
-      });
-    });
-    it('should handle AUTH_API_CALL_SUCCESS', () => {
-      store.dispatch({ type: 'API_CALL_SUCCESS' });
-      expect(store.getState().auth).toEqual({
-        currentUser: null,
-        isAuthenticated: false,
-        loading: true,
-        error: null,
-      });
-    });
-    it('should handle REGISTER_SUCCESS', () => {
-      store.dispatch({
-        type: 'REGISTER_USER_SUCCESS',
-        payload: { name: 'user1' },
-      });
-      expect(store.getState().auth).toEqual({
-        currentUser: { name: 'user1' },
-        isAuthenticated: false,
-        loading: false,
-        error: null,
-      });
-    });
-  
-    it('should handle REGISTER_FAILURE', () => {
-      store.dispatch({ type: 'REGISTER_USER_FAIL', payload: 'Signup failed' });
-      expect(store.getState().auth).toEqual({
-        currentUser: { name: 'user1' },
-        isAuthenticated: false,
-        loading: false,
-        error: 'Signup failed',
-      });
-    });
-  
-    it('should not handle LOAD_COURSE_SUCCESS', () => {
-      store.dispatch({ type: 'LOAD_TEACHERS_SUCCESS' });
-      expect(store.getState().auth).toEqual({
-        currentUser: { name: 'user1' },
-        isAuthenticated: false,
-        loading: false,
-        error: 'Signup failed',
-      });
-    });
-  
-    it('should handle LOGIN_SUCCESS action', () => {
-      store.dispatch({
-        type: 'LOGIN_USER_SUCCESS',
-        payload: { name: 'user1' },
-      });
-      expect(store.getState().auth).toEqual({
-        currentUser: { name: 'user1' },
-        isAuthenticated: true,
-        loading: false,
-        error: 'Signup failed',
-      });
+  it('should return the initial state', () => {
+    expect(store.getState().auth).toEqual({
+      currentUser: null,
+      isAuthenticated: false,
+      loading: false,
+      error: null,
     });
   });
-  
+  it('should handle AUTH_API_CALL_SUCCESS', () => {
+    store.dispatch({ type: 'API_CALL_SUCCESS' });
+    expect(store.getState().auth).toEqual({
+      currentUser: null,
+      isAuthenticated: false,
+      loading: true,
+      error: null,
+    });
+  });
+  it('should handle REGISTER_SUCCESS', () => {
+    store.dispatch({
+      type: 'REGISTER_USER_SUCCESS',
+      payload: { name: 'user1' },
+    });
+    expect(store.getState().auth).toEqual({
+      currentUser: { name: 'user1' },
+      isAuthenticated: false,
+      loading: false,
+      error: null,
+    });
+  });
+
+  it('should handle REGISTER_FAILURE', () => {
+    store.dispatch({ type: 'REGISTER_USER_FAIL', payload: 'Signup failed' });
+    expect(store.getState().auth).toEqual({
+      currentUser: { name: 'user1' },
+      isAuthenticated: false,
+      loading: false,
+      error: 'Signup failed',
+    });
+  });
+
+  it('should not handle LOAD_COURSE_SUCCESS', () => {
+    store.dispatch({ type: 'LOAD_TEACHERS_SUCCESS' });
+    expect(store.getState().auth).toEqual({
+      currentUser: { name: 'user1' },
+      isAuthenticated: false,
+      loading: false,
+      error: 'Signup failed',
+    });
+  });
+
+  it('should handle LOGIN_SUCCESS action', () => {
+    store.dispatch({
+      type: 'LOGIN_USER_SUCCESS',
+      payload: { name: 'user1' },
+    });
+    expect(store.getState().auth).toEqual({
+      currentUser: { name: 'user1' },
+      isAuthenticated: true,
+      loading: false,
+      error: 'Signup failed',
+    });
+  });
+});
--- src/_tests_/reduxTests/TeachersReducer.test.js
+++ src/_tests_/reduxTests/TeachersReducer.test.js
@@ -1,53 +1,55 @@
-// import createStore from '../../redux/createStore';
+import createStore from '../../redux/createStore';
 
-// const store = createStore();
+const store = createStore();
 
-// describe('Teacher reducer', () => {
-//     it('should return the initial state', () => {
-//       expect(store.getState().teachers).toEqual({
-//         error: null,
-//         list: [],
-//         loading: false,
-//       });
-//     });
-  
-//     it('should handle the LOAD_TEACHERS_SUCCESS action', () => {
-//       const teachers = [
-//         {
-//           name: 'john doe',
-//           courses: 'ruby',
-//           experience: '7years',
-//           image: ('https://image.freepik.com/free-photo/senior-male-p…aining-writing-green-chalkboard_23-2148200956.jpg'),
-//         },
-//       ];
-  
-//       const action = {
-//         type: 'LOAD_TEACHERS_SUCCESS',
-//         payload: teachers,
-//       };
-  
-//       store.dispatch(action);
-  
-//       expect(store.getState().teachers.list).toEqual(teachers);
-//     });
-  
-//     it('should handle the LOAD_TEACHERS_FAILURE action', () => {
-//       const error = 'Error';
-  
-//       const action = {
-//         type: 'LOAD_TEACHERS_FAIL',
-//         payload: error,
-//       };
-  
-//       store.dispatch(action);
-//       expect(store.getState().teachers.error).toEqual(error);
-//     });
-  
-//     it('should throw an error when dispatching invalid action', () => {
-//       expect(() => {
-//         store.dispatch({ name: 'INVALID_ACTTION' });
-//       }).toThrow(
-//         'Actions may not have an undefined "type" property. You may have misspelled an action type string constant.',
-//       );
-//     });
-//   });
+describe('Teacher reducer', () => {
+  it('should return the initial state', () => {
+    expect(store.getState().teachers).toEqual({
+      error: null,
+      list: [],
+      loading: false,
+    });
+  });
+
+  it('should handle the TEACHERS_SUCCESS action', () => {
+    const teachers = [
+      {
+        id: 1,
+        name: 'john doe',
+        courses: 'Poetry',
+        experience: '7yrs',
+        image:
+          'https://image.freepik.com/free-photo/senior-male-p…aining-writing-green-chalkboard_23-2148200956.jpg',
+      },
+    ];
+
+    const action = {
+      type: 'LOAD_TEACHERS_SUCCESS',
+      payload: teachers,
+    };
+
+    store.dispatch(action);
+
+    expect(store.getState().teachers.list).toEqual(teachers);
+  });
+
+  it('should handle the LOAD_TEACHERS_FAILURE action', () => {
+    const error = 'Error';
+
+    const action = {
+      type: 'LOAD_TEACHERS_FAIL',
+      payload: error,
+    };
+
+    store.dispatch(action);
+    expect(store.getState().teachers.error).toEqual(error);
+  });
+
+  it('should throw an error when dispatching invalid action', () => {
+    expect(() => {
+      store.dispatch({ name: 'INVALID_ACTTION' });
+    }).toThrow(
+      "Actions may not have an undefined type' property. You may have misspelled an action type string constant.",
+    );
+  });
+});
